bdc94a2d8463b13e00ba4fea21e75ef601d5788a
wizard <TrinUkWoLoz@triniwizard@gmail.com>
2020-03-11 08:23:32 +0000

    Adding code_play.py and updating notes

---
+++ code_play.py
@@ -0,0 +1,17 @@
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+
+var = "3.7.1"
+print("Python version: " + var)
+
+john = 3
+mary = 5
+adam = 6
+
+print(john , mary , adam)
+
+totalApples = john + mary + adam
+print(totalApples)
