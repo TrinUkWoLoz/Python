afb437271887e51b31f7ad594749cefc1bfdd304
root <triniwizard@gmail.com>
2020-03-17 10:58:34 +0000

    New code play and addition to notes

--- Notes.py
+++ Notes.py
@@ -326,6 +326,24 @@ else: larger_number = number2
 # print the result
 print("The larger number is:", larger_number)
 
+#Write a program that utilizes the concept of conditional execution, takes a string as input, and:
+
+#prints the sentence "Yes - Spathiphyllum is the best plant ever!" to the screen if the inputted string is "Spathiphyllum" (upper-case)
+#prints "No, I want a big Spathiphyllum!" if the inputted string is "spathiphyllum" (lower-case)
+#prints "Spathiphyllum! Not [input]!" otherwise. Note: [input] is the string taken as input.
+
+plant = input("Type in a really cool plant: ")
+
+if plant == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+
+elif plant == "spathiphyllum":
+    print("No, I want a big Spathiphyllum")
+
+else:
+    print("Spathiphyllum, not ", plant)
+
+    
 
 
 
--- code_play_1.py
+++ code_play_1.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
-input()
+#Write a program that utilizes the concept of conditional execution, takes a string as input, and:
+
+#prints the sentence "Yes - Spathiphyllum is the best plant ever!" to the screen if the inputted string is "Spathiphyllum" (upper-case)
+#prints "No, I want a big Spathiphyllum!" if the inputted string is "spathiphyllum" (lower-case)
+#prints "Spathiphyllum! Not [input]!" otherwise. Note: [input] is the string taken as input.
+
+plant = input("Type in a really cool plant: ")
+
+if plant == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+
+elif plant == "spathiphyllum":
+    print("No, I want a big Spathiphyllum")
+
+else:
+    print("Spathiphyllum, not ", plant)
+
+
