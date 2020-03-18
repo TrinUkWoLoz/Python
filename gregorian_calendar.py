382c5d005de14728846680818e842e084b3059b5
root <triniwizard@gmail.com>
2020-03-18 09:11:46 +0000

    Addition to calendar and tax scripts

--- gregorian_calendar.py
+++ gregorian_calendar.py
@@ -4,6 +4,16 @@
 #otherwise, if the year number isn't divisible by 100, it's a leap year;
 #otherwise, if the year number isn't divisible by 400, it's a common year;
 #otherwise, it's a leap year
-
 year = int(input("Enter a year: "))
+
+if year < 1582:
+    print("Not within the Gregorian calendar period")
+elif year % 4 !=0:
+    print("common year")
+elif year % 100 !=0:
+    print("leap year")
+elif year % 400 !=0:
+    print("common year again")
+else:
+    print("leap year")
 
