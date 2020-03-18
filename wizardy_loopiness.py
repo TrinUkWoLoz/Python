8ae88e9c20ea0247c3d7526cfb1e1e8424ae3895
root <triniwizard@gmail.com>
2020-03-18 16:30:51 +0000

    Loopiness added and Notes updated

---
+++ wizardy_loopiness.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.7
+
+secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+# Prompt the user to enter an integer number.
+input("Guess a number you muggle chimp:")
+
+while secret_number != 777:
+    print("Haha your stuck in my infinite loop Harry Potter (Dumbass)")
+    if secret_number == 777:
+        print("Well done, muggle! You are free now.")
+
+
