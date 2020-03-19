e8e2142201af255d154dc117f37e65142dae2fa5
root <triniwizard@gmail.com>
2020-03-19 10:53:42 +0000

    New script and addition to Notes

--- Notes.py
+++ Notes.py
@@ -456,6 +456,26 @@ for i in range(2, 8):
 for i in range(2, 8, 3):
     print("The value of i is currently", i)
 
+#break + continue example - input numbers until -1 is entered and then display highest number inputted
+largestNumber = -99999999
+counter = 0
+
+number = int(input("Enter a number or type -1 to end program: "))
+
+while number != -1:
+    if number == -1:
+        continue            #or break
+    counter += 1
+
+    if number > largestNumber:
+        largestNumber = number
+    number = int(input("Enter a number or type -1 to end program: "))
+
+if counter:
+    print("The largest number is", largestNumber)
+else:
+    print("You haven't entered any number.")
+
 
 
 
--- for_i_loop.py
+++ for_i_loop.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3.7
 #Count to 5 Mississippi's and then say here I come
 
-for i in range(1, 6):
-    print(i, "Mississippi")
-print("Ready or not, here I come!")
+import time
 
+for i in range(1,6):
+    print(i,"Mississippi")
+    time.sleep(1)
+print("Ready or not, here I come!")
