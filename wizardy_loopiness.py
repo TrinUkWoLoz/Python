9ab6e9fdac75d344161f3e9ed7433fc00af37f8b
root <triniwizard@gmail.com>
2020-03-19 09:18:10 +0000

    Addition to scripts

--- wizardy_loopiness.py
+++ wizardy_loopiness.py
@@ -4,20 +4,20 @@ secret_number = 777
 
 print(
 """
-+================================+
-| Welcome to my game, muggle!    |
-| Enter an integer number        |
-| and guess what number I've     |
-| picked for you.                |
-| So, what is the secret number? |
-+================================+
++==========================================+
+| Welcome to my game, potential muggle!    |
+| Enter an integer number                  |
+| and guess what magic number I've         |
+| picked for you.                          |
+| So, what is the magic number?            |
++==========================================+
 """)
 
-input_num = int(input("Guess a number you muggle chimp: "))
+input_num = int(input("Guess a number to see if your magic: "))
 
 while input_num != secret_number:
-    print("Haha your stuck in my infinite loop Harry Potter (Dumbass)")
+    print("Haha your stuck in my infinite loop you muggle chimp")
 if input_num == secret_number:
-    print("Well done, muggle! You are free now.")
+    print("Well done Harry Potter! You are free now. Welcome to Hogwarts.")
 
 
