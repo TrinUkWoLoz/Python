9cc053b0c462028b0a79a5243ee4a8cd7ac15628
appliance <triniwizard@gmail.com>
2020-04-01 09:44:16 +0100

    Auto add, commit and push script

--- auto_git_push_Python.sh
+++ auto_git_push_Python.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #This has been written for auto add commit and push from asus laptop to master repo
-git add --all
-git commit -m "Auto add, commit and push script"
-git push
+git add --all && git commit -m "Auto add, commit and push script" && git push
+
+#This clause is for sudo push
+sudo git add --all && sudo git commit -m "Auto add, commit and push script" && sudo git push
