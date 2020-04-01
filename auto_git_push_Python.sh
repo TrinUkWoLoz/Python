85597944cdda811b1a64ef549fb60082cbd5503c
appliance <triniwizard@gmail.com>
2020-04-01 09:46:46 +0100

    Auto add, commit and push script

--- auto_git_push_Python.sh
+++ auto_git_push_Python.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-#This has been written for auto add commit and push from asus laptop to master repo
-git add --all && git commit -m "Auto add, commit and push script" && git push
+##This has been written for auto add commit and push from asus laptop to master repo
+#git add --all && git commit -m "Auto add, commit and push script" && git push
+#
+##This clause is for sudo push
+#sudo git add --all && sudo git commit -m "Auto add, commit and push script" && sudo git push
 
-#This clause is for sudo push
-sudo git add --all && sudo git commit -m "Auto add, commit and push script" && sudo git push
+
+git add --all && git commit -m "Auto add, commit and push script" && git push
+if [ "$?" -eq "0" ]
+then
+  echo "Sudoless command successful"
+else
+  echo "Sudoless command failed, now running as root"
+  sudo git add --all && sudo git commit -m "Auto add, commit and push script" && sudo git push
+fi
