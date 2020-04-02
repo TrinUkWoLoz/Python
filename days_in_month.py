64b4ffd4062bc31a0211f6c508be1bb71a4dfeae
appliance <triniwizard@gmail.com>
2020-04-02 16:12:42 +0100

    Improved days in month script

---
+++ days_in_month.py
@@ -0,0 +1,42 @@
+def isYearLeap(year):
+	if year%4==0:
+			if year%400!=0 and year%100==0:
+				return False
+			return True
+	else:
+		return False
+
+month_with_31days = [1,3,5,7,8,10,12]
+month_with_30days = [4,6,9,11]
+
+def daysInMonth(year, month):
+	if month in month_with_31days:
+		return 31
+	elif month in month_with_30days:
+		return 30
+	elif month == 2 and isYearLeap(year)==True:
+		return 29
+	elif month == 2 and isYearLeap(year)==False:
+		return 28
+	#if wrong month is provided returns None
+	else:
+		return None
+
+def dayOfYear(year, month, day):
+	#prevents incorrect month input using previous function
+	if daysInMonth(year,month)==None:
+		return None
+	#prevents incorrect day input checking if there is such day in given month:
+	if day<=0 or day > daysInMonth(year,month):
+		return None
+	#calculations:
+	sum_of_days=0
+	#for loop that adds all full months
+	for m in range(month-1):
+		sum_of_days += daysInMonth(year,m+1)
+	#adding days from last month
+	sum_of_days = sum_of_days + day
+	return sum_of_days
+
+print(dayOfYear(2020, 4, 2))
+
--- leap_year_check.py
+++ leap_year_check.py
@@ -1,21 +1,31 @@
 # Script to check if values (years) in list are leap years
 
 def isYearLeap(year):
+# A leap year is every 4 years, but not every 100 years, then again every 400 years
     return (((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0));
 
-testData = [1900, 2000, 2016, 1987]
-testResults = [False, True, True, False]
+# Desired years to check
+testData = [1900, 2000, 2016, 1987, 2020]
+# Results of above calcs on values in list
+testResults = [False, True, True, False, True]
+
 for i in range(len(testData)):
     yr = testData[i]
     print(yr, "->", end="")
     result = isYearLeap(yr)
-    if result == testResults[i]:
-        print("OK")
+    # if result == testResults[i]:
+    #     print("OK")
+    # else:
+    #     print("Failed")
+    if testResults[i] == True:
+        print("This a leap year you fantastically clever beast!")
     else:
-        print("Failed")
+        print("This ain't no leap year chump muda *******!")
+
+######WORKINGS#########
 
 # 1900               ((True and False) or False)
-#                        False or Flase
+#                        False or False
 #                              False
 
 # 2000               ((True and False) or True)
@@ -29,3 +39,9 @@ for i in range(len(testData)):
 # 1987               ((False and True) or False)
 #                          False or False
 #                               False
+
+# 2020               ((True and True) or False)
+#                          True or False
+#                               True
+
+
