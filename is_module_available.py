2d245c7b4ad429cccaacd919d3f1786cd5774da0
wizard <TrinUkWoLoz@triniwizard@gmail.com>
2020-04-12 14:10:58 +0100

    Script requires changing of module value to work

--- is_module_available.py
+++ is_module_available.py
@@ -1,8 +1,20 @@
+# Script designed to detect if a module is available for import
+
 try:
     import pefile
+
+#    module_to_test = input("Enter the module to see if available you chump:\n", )
+#    import module_to_test
+# except ImportError:
+
 except ImportError:
     available = False
+    print("Module not present you pheasant")
 else:
     available = True
+    print("Module is locked and loaded")
+
+# print("Module check to see if string is correct:", module_to_test)
+
 
 
