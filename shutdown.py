95bd79a9fad60749bd8c81bfd6d4e5120aac1621
caroni <triniwizard@gmail.com>
2020-07-13 11:00:09 +0100

    Latest additions to repo

---
+++ shutdown.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.7
+
+import os
+
+shutdown = input("Sir Wizard, would you like to shutdown your device(yes/no): ")
+if shutdown == 'no':
+    exit()
+else:
+    os.system("shutdown /s /t 1")
+
+
