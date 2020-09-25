5be0ca40f3f9b05ae443e0e03bf1b508f187c6af
caroni <triniwizard@gmail.com>
2020-09-25 18:26:30 +0100

    Added file illustrating concatenation

--- concatenation.py
+++ concatenation.py
@@ -1,4 +1,5 @@
 #/usr/bin/python3.7
 
 print("Hello, this is a simple example of concatenation with the use of the plus sign, " +
-      "which allows this below line to be joined with the above, with the use of concatenation.")
+      "which allows this below line to be MERGED with the above, " +
+      "concatenation = a group of things linked together or occurring together in a way that produces a particular result.")
