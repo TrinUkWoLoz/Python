0da328f2a631bfbaa4a9bc02643e8e5153171131
caroni <triniwizard@gmail.com>
2020-09-25 18:38:41 +0100

    multiple line comment example

---
+++ multiple_line_comments.py
@@ -0,0 +1,9 @@
+#/usr/bin/python3.7
+
+print("Code shows the use of multiple line comments e.g using 3 consecutive speech marks above and below the comment ")
+
+"""
+    Application: multiple_line_comments.py
+    Written by: Wizard 
+    Purpose: Shows the use of multiple line comments
+"""
