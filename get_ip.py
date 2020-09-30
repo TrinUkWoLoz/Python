8ef434b64aace3f57bb6193022cab3f244e572a5
caroni <triniwizard@gmail.com>
2020-09-30 10:05:39 +0100

    Amended argument help message

--- get_ip.py
+++ get_ip.py
@@ -3,19 +3,11 @@ import argparse
 import socket
 
 parser = argparse.ArgumentParser()
-parser.add_argument('url', help="Check a url for straight quotes", type=str)
+parser.add_argument('url', help="Input URL without http/s:// e.g google.com", type=str)
 args = parser.parse_args()
-#print(args.url)
 
 def url_to_ip():
     print(socket.gethostbyname(args.url))
 
 if url_to_ip() is not None:
     print(url_to_ip())
-
-
-
-
-
-
-
